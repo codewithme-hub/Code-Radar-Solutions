f66efb086d456ccac0d617c360189878f854e442
codewithme-hub <ewild134@gmail.com>
2025-01-25 18:15:31 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -18,7 +18,7 @@ int main() {
             printf("%d\n", num1 * num2);
             break;
         case '/':
-            printf("%d\n", num1 / num2)
+            printf("%d\n", num1 / num2);
             break;
         default:
             printf("error\n");
