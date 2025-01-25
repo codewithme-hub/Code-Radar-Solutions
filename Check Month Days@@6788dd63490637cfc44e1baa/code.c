9868718c1c4d7b8b8472c5b54b929653a7a7f6b5
codewithme-hub <ewild134@gmail.com>
2025-01-25 18:45:02 +0530

    Solution to Check Month Days

--- Check Month Days@@6788dd63490637cfc44e1baa/code.c
+++ Check Month Days@@6788dd63490637cfc44e1baa/code.c
@@ -1,36 +1,47 @@
 #include <stdio.h>
 int main() {
-    int num;
-    char choice;
-    scanf("%d",&num);
-    scanf("%c",&choice);
-    switch(choice){
+    int month;
+    scanf("%d",&month);
+    switch(month){
         case '1':
-            printf("January\n");
+            printf("31\n");
+            break; //jan
         case '2':
-            printf("Febuary\n");
+            printf("28\n");
+            break; //feb
         case '3':
-            printf("March\n");
+            printf("31\n");
+            break; //march
         case '4':
-            printf("April\n");
+            printf("30\n");
+            break; //april
         case '5':
-            printf("May\n");
+            printf("31\n");
+            break; //may
         case '6':
-            printf("June\n");
+            printf("30\n");
+            break;//june
         case '7':
-            printf("July\n");
+            printf("31\n");
+            break;//july
         case '8':
-            printf("August\n");
+            printf("31\n");
+            break;//august
         case '9':
-            printf("September\n");
+            printf("30\n");
+            break;//sep
         case '10':
-            printf("October\n");
+            printf("31\n");
+            break;//oct
         case '11':
-            printf("November\n");
+            printf("30\n");
+            break;//nov
         case '12':
-            printf("December\n");
+            printf("31\n");
+            break;//dec
         default:
             printf("Invalid month\n");
+            break;
     }
     
     return 0;
